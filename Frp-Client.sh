f9c100a109b8f8791691dd107b27e5e8b3f1e849
Lee Linus <708863861@qq.com>
2017-05-18 23:04:11 +0800

    Update Frp-Client.sh

--- Frp-Client.sh
+++ Frp-Client.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 green='\e[1;32m' # green
 red='\e[1;31m' # red
 blue='\e[1;34m' # blue  
@@ -13,8 +12,9 @@ download(){
 	echo -n "下载FRP . . ."
 	sleep 0.3 
 	echo -n "."
-	mkdir /root/frp
-	wget -O /root/frp/frp.tar.gz $1
+	rm -rf /etc/frp/ >/dev/null 2>&1  
+	mkdir /etc/frp
+	wget -O /etc/frp/frp.tar.gz $1
 	if [ $? == 0 ] ; then
 		echo -e "[${green}成功${nc}]"
 	else
@@ -26,17 +26,17 @@ download(){
 install(){
 	echo -n "Installing Frp . . ."
 	pcd=`pwd`
-	cd /root/frp/
+	cd /etc/frp/
 	sleep 0.3
 	echo -n "."
-	tar zxf /root/frp/frp.tar.gz 
+	tar zxf /etc/frp/frp.tar.gz 
 	if [ $? == 0 ] ; then
 		echo -e "[${green}成功${nc}]"
 	else
 		echo -e "[${red}失败${nc}]"
 		exit 1
 	fi
-	rm -f /root/frp/frp.tar.gz
+	rm -f /etc/frp/frp.tar.gz
 	mv frp_0.* frp-config
 	cd $pcd
 }
@@ -55,44 +55,81 @@ ______           _
 欢迎使用FRP内网映射脚本
 =========================================================================
 Author: Mr Funky <QQ:708863861>
-版本更新：采用0.93版本搭建，新增开启启动功能
+版本更新：更新启动脚本功能,断线重连功能
 =========================================================================
 USER: $USER   HOST: $HOSTNAME  KERNEL: `uname -r`  
-DISK :`ls  /dev/sd? 2>/dev/null`
+DISK :`ls  /dev/?d? 2>/dev/null`
 你确定要安装吗？【Y/y】
 EOF
 config(){
 	echo -n "设置FRP Frp . . ."
 	pcd=`pwd`
-	cd /root/frp/frp-config
+	cd /etc/frp/frp-config
 	sleep 0.3
 	echo -n "."
 	rm -f frpc.ini
 	touch frpc.ini
+	while (true) do
 	read -p  "请输入FRP服务端密码:[geekfan.top123]" password
 	if [ ! $password ] ;then
 		password=geekfan.top123
 	fi
+	read -p  "确认密码: $password ? [y/n]" yn
+	if [ $yn == 'Y' -o $yn == 'y' ] ;then
+		break
+	fi
+	done
+	while (true) do
 	read -p  "请输入FRP服务端域名:[www.funkystars.cn]" IP
 	if [ ! $IP ] ;then
 		IP=www.funkystars.cn
 	fi
-	read -p  "请输入本地IP:[127.0.0.1]" Local
+	read -p  "确认FRP服务端域名: $IP ? [y/n]" yn
+	if [ $yn == 'Y' -o $yn == 'y' ] ;then
+		break
+	fi
+	done
+	while (true) do
+	read -p  "请输入本地IP:[127.0.0.1] ? " Local
 	if [ ! $Local ] ;then
 		Local=127.0.0.1
 	fi
+	read -p  "确认本地IP: $Local ? [y/n] " yn
+	if [ $yn == 'Y' -o $yn == 'y' ] ;then
+		break
+	fi
+	done
+	while (true) do
 	read -p  "请输入FRP客户端域名:[www.youname.com]" Domain
 	if [ ! $Domain ] ;then
 		Domain=www.youname.com
 	fi	
+	read -p  "确认客户端域名: $Domain ? [y/n] " yn
+	if [ $yn == 'Y' -o $yn == 'y' ] ;then
+		break
+	fi
+	done
+	while (true) do
 	read -p  "请设置HTTP端口隧道名称:[$rand1]" HTTP
 	if [ ! $HTTP ] ;then
 		HTTP=$rand1
 	fi
+	read -p  "确认HTTP端口隧道名称: $HTTP ? [y/n]" yn
+	if [ $yn == 'Y' -o $yn == 'y' ] ;then	
+		break
+	fi
+	done
+	while (true) do
 	read -p  "请设置SSH端口隧道名称(默认SSH端口):[$rand2]" SSH
 	if [ ! $SSH ] ;then
 		SSH=$rand2
 	fi
+	read -p  "确认SSH端口隧道名称: $SSH ? [y/n] " yn
+	if [ $yn == 'Y' -o $yn == 'y' ] ;then
+		break
+	fi
+	done
+	
 cat > frpc.ini <<EOF
 [common]
 server_addr = $IP
@@ -117,6 +154,73 @@ local_ip = $Local
 local_port = 22
 use_gzip = true use_encrypti
 EOF
+
+sudo cat > /etc/init.d/frp <<EOF
+#!/bin/bash
+# chkconfig: - 99 2
+# description: FRP Client Control Script
+green='\e[1;32m' 
+red='\e[1;31m' 
+nc='\e[0m'
+PIDF=\` ps  -A | grep frpc | awk '{print \$1}'\`
+PIDK=\` ps  -A | grep keepalived | awk '{print \$1}'\`
+case "\$1" in
+        start)
+		echo -n "FRP 正在启动..."
+        nohup /etc/frp/frp-config/frpc -c /etc/frp/frp-config/frpc.ini >/dev/null 2>&1  &
+		RE=\$?
+		nohup /etc/frp/frp_keepalived >/dev/null 2>&1  &
+		if [ \$? -eq 0 -o \$RE -eq 0 ] 2> /dev/null ; then
+				sleep 1
+				echo -e "[\${green}成功\${nc}]"
+		else 
+				echo -e "[\${red}失败\${nc}]"
+		fi
+        ;;
+        stop)
+		echo -n "FRP 正在停止..."
+        kill -9 \$PIDF
+		RE=\$?
+		kill -9  \$PIDK
+		if [ \$? -eq 0 -o \$RE -eq 0 ] 2> /dev/null ; then
+				sleep 1
+				echo -e "[\${green}成功\${nc}]"
+		else 
+				echo -e "[\${red}失败\${nc}]"
+		fi		
+        ;;
+        restart)
+        \$0 stop &> /dev/null
+        if [ \$? -ne 0 ] 2> /dev/null ; then continue ; fi
+        \$0 start
+        ;;
+        reload)
+		echo -n "FRP 正在平滑重启..."
+        kill -1 \$PIDF
+		RE=\$?
+		kill -1 \$PIDK
+		if [ \$? -eq 0 -o \$RE -eq 0 ] 2> /dev/null ; then
+				sleep 1
+				echo -e "[\${green}成功\${nc}]"
+		else 
+				echo -e "[\${red}失败\${nc}]"
+		fi
+        ;;
+		status)
+		echo "FRP 当前状态:"
+		cat /frpc.log
+		;;
+        *)
+        echo "Userage: \$0 { start | stop | restart | reload | status }"
+        exit 1
+esac
+exit 0
+
+EOF
+
+
+chmod +x /etc/init.d/frp
+
 	if [ $? == 0 ] ; then
 		echo -e "[${green}成功${nc}]"
 	else
@@ -125,7 +229,20 @@ EOF
 	fi
 	cd $pcd
 }
-
+frp_keepalived(){
+cat > /etc/frp/frp_keepalived << EOF
+#!/bin/bash
+while  :
+do
+	PIDF=\` ps  -A | grep frpc | awk '{print \$1}'\`
+	if [ \$PIDF -eq 0 ];then
+	   nohup /etc/frp/frp-config/frpc -c /etc/frp/frp-config/frpc.ini >/dev/null 2>&1  &	
+	fi
+	sleep 60
+done
+EOF
+chmod +x /etc/frp/frp_keepalived
+}
 
 
 read -p "请输入Y/y确定安装" key
@@ -138,7 +255,7 @@ case $key in
 (5)Mitps64 		(6)Mitpsle
 (7)Mitps64le 
 (8)	如果已经下载好了FRP 
-	请将下载好的文件保存到/root/frp下
+	请将下载好的文件保存到/etc/frp下
 	并重命名为frp.tar.gz
 
 EOF
@@ -174,29 +291,18 @@ EOF
 			esac
 			install
 			config
+			frp_keepalived
 			result=$?
 			echo -n "FRP客户端安装结果  .."
 			if [ $result == 0 ] ; then
 				echo -e "[${green}成功${nc}]"
 				echo "Enjoy~"
 				sleep 1
-				echo "你可以在/frp/frp-config目录下通过: \"./frpc -c ./frpc.ini\" 命令去启动FRP，SSH远程端口默认为11 "
-				read -p "是否设置开机启动？Y/y[y]" key
-				case $key in 
-				"y"|"Y"|"")
-						chmod +x /etc/rc.d/rc.local
-						sed -i  '$i\/root/frp/frp-config/frpc -c /root/frp/frp-config/frpc.ini'  /etc/rc.d/rc.local
-						if [ $? == 0 ] ; then
-								echo -e "[${green}成功${nc}]"
-						else
-								echo -e "[${red}失败${nc}]"
-								exit 1
-						fi
-				;;
-				*)
-				;;
-				esac
-				sleep 1
+				echo "你可以通过: \"service frp start\" 命令去启动FRP"
+				echo "通过\"chkconfig frp on\" 设置FRP开机启动,  SSH远程端口默认为$rand2 "
+				echo "HTTP隧道默认为$rand2 ,FRP 配置文件路径 /etc/frp/frp-config/frpc.ini"
+				echo "有任何问题,通过 \"service frp status\"查看frp工作状态,解决"
+				read -p "按ENTER键退出安装"
 				exit
 			else
 				echo -e "[${red}失败${nc}]"
